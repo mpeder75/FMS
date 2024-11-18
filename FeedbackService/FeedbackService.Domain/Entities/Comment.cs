284ec104bc332aa08ba3957cfa2c98ab39a13ce4
Bolchefar <Frederik@kasten.dk>
2024-11-18 12:03:05 +0100

    Moved the comment class to entities instead of ValueObjects

--- FeedbackService/FeedbackService.Domain/Entities/Comment.cs
+++ FeedbackService/FeedbackService.Domain/Entities/Comment.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FeedbackService.Domain.ValueObjects
+namespace FeedbackService.Domain.Entities
 {
     public class Comment
     {
--- FeedbackService/FeedbackService.Domain/Entities/Feedbackpost.cs
+++ FeedbackService/FeedbackService.Domain/Entities/Feedbackpost.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using FeedbackService.Domain.ValueObjects;
 using SharedKernel.Entities;
 
 namespace FeedbackService.Domain.Entities
