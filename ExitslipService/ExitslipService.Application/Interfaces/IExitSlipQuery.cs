5c660e5661bb43fc9ebb16e956fd8dfd9a5f8dcb
MarshesAndMeadows <khin58394@edu.ucl.dk>
2024-11-25 11:03:06 +0100

    merge aah moment 2.0

---
+++ ExitslipService/ExitslipService.Application/Interfaces/IExitSlipQuery.cs
@@ -0,0 +1,15 @@
+﻿using ExitslipService.Application.Query.ExitSlipDTO;
+using ExitslipService.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExitslipService.Application.Interfaces
+{
+    public interface IExitSlipQuery
+    {
+        ExitSlipDTO Get(Guid id);
+    }
+}
