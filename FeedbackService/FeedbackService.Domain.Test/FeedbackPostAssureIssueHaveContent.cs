d605215ba5eb0d7d7ba0b6fcca008062f56d679c
Matias Møller Hansen <mmha59221@edu.ucl.dk>
2024-12-02 21:19:40 +0100

    Unittest done

---
+++ FeedbackService/FeedbackService.Domain.Test/FeedbackPostAssureIssueHaveContent.cs
@@ -0,0 +1,66 @@
+﻿
+using FeedbackService.Domain.Test.Fakes;
+
+namespace FeedbackService.Domain.Test
+{
+    public class FeedbackPostAssureIssueHaveContent
+    {
+// ------------- Testing success scenario -------------
+        [Fact] 
+        public void Given_Issue_Have_Content__Then_Dont_Throw_Exception()
+        {
+            // Arrange
+            var roomId = Guid.NewGuid();
+            var authorId = Guid.NewGuid();
+            var title = "Some title";
+            var issueText = "Valid issue text";
+            var solutionText = "Some solution text";
+            var createdAt = DateTime.Now;
+
+            // Valid Issue
+            var feedbackPost = new FakeFeedbackPost(roomId, authorId, title, issueText, solutionText, createdAt);
+
+            // Act & Assert
+            feedbackPost.AssureIssueHaveContent();
+        }
+
+// ------------- Testing alternative paths -------------
+        [Fact] 
+        public void Given_Issue_Is_Whitespace__Then_Throw_Exception()
+        {
+            // Arrange
+            var roomId = Guid.NewGuid();
+            var authorId = Guid.NewGuid();
+            var title = "Some title";
+            var issueText = " ";
+            var solutionText = "Some solution text";
+            var createdAt = DateTime.Now;
+
+            // Use an empty Issue to trigger the exception
+            var feedbackPost = new FakeFeedbackPost(roomId, authorId, title, issueText, solutionText, createdAt);
+
+            // Act & Assert
+            var exception = Assert.Throws<Exception>(() => feedbackPost.AssureIssueHaveContent());
+            Assert.Equal("Describe an issue.", exception.Message);
+        }
+
+        [Fact]
+        public void Given_Issue_Is_Empty__Then_Throw_Exception()
+        {
+            // Arrange
+            var roomId = Guid.NewGuid();
+            var authorId = Guid.NewGuid();
+            var title = "Some title";
+            var issueText = "";
+            var solutionText = "Some solution text";
+            var createdAt = DateTime.Now;
+
+            // Use an empty Issue to trigger the exception
+            var feedbackPost = new FakeFeedbackPost(roomId, authorId, title, issueText, solutionText, createdAt);
+
+            // Act & Assert
+            var exception = Assert.Throws<Exception>(() => feedbackPost.AssureIssueHaveContent());
+            Assert.Equal("Describe an issue.", exception.Message);
+        }
+    }
+}
---
+++ FeedbackService/FeedbackService.Domain.Test/FeedbackPostAssureTitleHaveContent.cs
@@ -0,0 +1,65 @@
+﻿using FeedbackService.Domain.Test.Fakes;
+
+namespace FeedbackService.Domain.Test
+{
+    public class FeedbackPostAssureTitleHaveContent
+    {
+// ------------- Testing success scenario -------------
+        [Fact]
+        public void Given_Title_Have_Content__Then_Dont_Throw_Exception()
+        {
+            // Arrange
+            var roomId = Guid.NewGuid();
+            var authorId = Guid.NewGuid();
+            var title = "Valid title";
+            var issueText = "Some issue text";
+            var solutionText = "Some solution text";
+            var createdAt = DateTime.Now;
+
+            // Valid title
+            var feedbackPost = new FakeFeedbackPost(roomId, authorId, title, issueText, solutionText, createdAt);
+
+            // Act & Assert
+            feedbackPost.AssureTitleHaveContent();
+        }
+        
+// ------------- Testing alternative paths -------------
+        [Fact]
+        public void Given_Title_Is_Whitespace__Then_Throw_Exception()
+        {
+            // Arrange
+            var roomId = Guid.NewGuid();
+            var authorId = Guid.NewGuid();
+            var title = " ";
+            var issueText = "Some issue text";
+            var solutionText = "Some solution text";
+            var createdAt = DateTime.Now;
+
+            // Use an empty title to trigger the exception
+            var feedbackPost = new FakeFeedbackPost(roomId, authorId, title, issueText, solutionText, createdAt);
+
+            // Act & Assert
+            var exception = Assert.Throws<Exception>(() => feedbackPost.AssureTitleHaveContent());
+            Assert.Equal("Add a Title.", exception.Message);
+        }
+
+        [Fact]
+        public void Given_Title_Is_Empty__Then_Throw_Exception()
+        {
+            // Arrange
+            var roomId = Guid.NewGuid();
+            var authorId = Guid.NewGuid();
+            var title = "";
+            var issueText = "Some issue text";
+            var solutionText = "Some solution text";
+            var createdAt = DateTime.Now;
+
+            // Use an empty title to trigger the exception
+            var feedbackPost = new FakeFeedbackPost(roomId, authorId, title, issueText, solutionText, createdAt);
+
+            // Act & Assert
+            var exception = Assert.Throws<Exception>(() => feedbackPost.AssureTitleHaveContent());
+            Assert.Equal("Add a Title.", exception.Message);
+        }
+    }
+}
