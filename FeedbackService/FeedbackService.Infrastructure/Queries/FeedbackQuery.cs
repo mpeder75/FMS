41dbbc7732d1b7eb2375d42bfc9a3bf08c2b5c68
Matias Møller Hansen <mmha59221@edu.ucl.dk>
2024-12-02 17:13:20 +0100

    Implementing report, need testing

--- FeedbackService/FeedbackService.Infrastructure/Queries/FeedbackQuery.cs
+++ FeedbackService/FeedbackService.Infrastructure/Queries/FeedbackQuery.cs
@@ -94,4 +94,33 @@ public class FeedbackPostQuery : IFeedbackPostQuery
         .ToListAsync();
         return feedbackPosts;
     }
+
+    async Task<IEnumerable<FeedbackPostDto>> IFeedbackPostQuery.GetFeedbackPostsByRoomAndDateAsync(Guid roomId, DateTime startDate, DateTime endDate)
+    {
+        var feedbackPosts = await _db.FeedbackPosts.AsNoTracking()
+        .Where(x => x.RoomId == roomId && x.CreatedAt >= startDate && x.CreatedAt <= endDate)
+        .Include(fp => fp.Comments)
+        .Select(x => new FeedbackPostDto
+        {
+            Id = x.Id,
+            RoomId = x.RoomId,
+            AuthorId = x.AuthorId,
+            Title = x.Title,
+            IssueText = x.IssueText,
+            SolutionText = x.SolutionText,
+            Likes = x.Likes,
+            Dislikes = x.Dislikes,
+            CreatedAt = x.CreatedAt,
+            Comments = x.Comments.Select(c => new CommentDto
+            {
+                Id = c.Id,
+                CommentString = c.CommentString,
+                CreatedAt = c.CreatedAt,
+                AuthorId = c.AuthorId
+            }).ToList()
+        })
+        .OrderByDescending(fp => fp.Comments.Count) // Only for demo
+        .ToListAsync();
+        return feedbackPosts;
+    }
 }
